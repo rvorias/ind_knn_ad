e95e7e79d1d04946e619f2f969876fbb19469591
rvorias <raphaelvorias@gmail.com>
2021-08-04 20:53:23 +0200

    Add docs and float16 support

--- indad/utils.py
+++ indad/utils.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from datetime import datetime
 
 import torch
-from torch import Tensor
+from torch import tensor
 from torchvision import transforms
 
 import numpy as np
@@ -26,16 +26,24 @@ class GaussianBlur:
         return final_map
 
 
-def get_coreset_idx_randomp(z_lib, n : int = 1000, eps : float = .95) -> Tensor:
+def get_coreset_idx_randomp(
+    z_lib : tensor, 
+    n : int = 1000,
+    eps : float = .9,
+    float16 : bool = True,
+    force_cpu : bool = False,
+) -> tensor:
     """Returns n coreset idx for given z_lib.
     
     Performance on AMD3700, 32GB RAM, RTX3080 (10GB):
-    CPU: 40-60 it/s, GPU: 1120 it/s
+    CPU: 40-60 it/s, GPU: 500+ it/s (float32), 1500+ it/s (float16)
 
     Args:
-        z_lib:  (n, d) tensor of patches.
-        n:      Number of patches to select.
-        eps:    Agression of the sparse random projection.
+        z_lib:      (n, d) tensor of patches.
+        n:          Number of patches to select.
+        eps:        Agression of the sparse random projection.
+        float16:    Cast all to float16, saves memory and is a bit faster (on GPU).
+        force_cpu:  Force cpu, useful in case of GPU OOM.
 
     Returns:
         coreset indices
@@ -54,7 +62,11 @@ def get_coreset_idx_randomp(z_lib, n : int = 1000, eps : float = .95) -> Tensor:
     # future reference.
     # min_distances = torch.sum(torch.pow(z_lib-last_item, 2), dim=1, keepdims=True)
 
-    if torch.cuda.is_available():
+    if float16:
+        last_item = last_item.half()
+        z_lib = z_lib.half()
+        min_distances = min_distances.half()
+    if torch.cuda.is_available() and not force_cpu:
         last_item = last_item.to("cuda")
         z_lib = z_lib.to("cuda")
         min_distances = min_distances.to("cuda")
